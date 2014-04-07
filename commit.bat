a5d6dc07018633c265b5d1d765f1891e3a45045d
michaelkrauty <michaelkrauty@gmail.com>
2014-04-07 11:43:42 -0700

    update

---
+++ commit.bat
@@ -0,0 +1,19 @@
+cd C:\Users\Michael\Desktop\test_server
+rmdir /Q /S git\FirstPvPFactions\src
+mkdir git\FirstPvPFactions\src
+copy plugins\FirstPvPFactions.jar git\FirstPvPFactions\src
+cd git\FirstPvPFactions\src
+jar xf FirstPvPFactions.jar
+rmdir /Q /S META-INF
+del .project
+del .classpath
+move FirstPvPFactions.jar ../target
+cd me\michaelkrauty\FirstPvPFactions
+del *.class
+del commands\*.class
+del util\*.class
+cd ../../../../
+git add --all
+git commit -m update
+git push
+PAUSE
