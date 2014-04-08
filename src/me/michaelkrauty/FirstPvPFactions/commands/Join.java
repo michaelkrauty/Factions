d3cfe6f706f30b0c14634bd3e0fe8abd675b83c9
michaelkrauty <michaelkrauty@gmail.com>
2014-04-08 11:09:16 -0700

    update

--- src/me/michaelkrauty/FirstPvPFactions/commands/Join.java
+++ src/me/michaelkrauty/FirstPvPFactions/commands/Join.java
@@ -9,11 +9,7 @@ public class Join {
 	public static void join(Player player, String[] args) {
 		if(args.length == 2){
 			String result = SQL.addPlayerToFaction(player.getUniqueId().toString(), args[1]);
-			if(result.equals("SUCCESS")){
-				player.sendMessage("success");
-			}else{
-				player.sendMessage(result);
-			}
+			player.sendMessage(result);
 		}
 	}
 
